7a6a54d5111c99dd87d455e80a20581b24d0c028
Doha Khan <doha123khan@gmail.com>
2026-07-16 00:59:23 +0600

    Initial Commit

---
+++ peekMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  peekMac
+//
+//  Created by Doha's Mac on 16/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ peekMac/peekMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  peekMacApp.swift
+//  peekMac
+//
+//  Created by Doha's Mac on 16/7/26.
+//
+
+import SwiftUI
+
+@main
+struct peekMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
